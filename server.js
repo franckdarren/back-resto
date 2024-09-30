e2daffec4c558c321018c8d206462650bb34f9e3
isaac omar <isaacomar29@gmail.com>
2024-10-01 00:56:37 +0100

    middleware d'authentification

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+const app = express();
+const port = 3000;
+
+// Définir une route
+app.get('/', (req, res) => {
+  res.send('salutations');
+});
+
+// Démarrer le serveur
+app.listen(port, () => {
+  console.log(`je fonctionne bien`);
+});
